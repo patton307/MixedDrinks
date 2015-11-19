a0fff141c69fb8d171957d20763004e84c556c1b
JackNel <jcneligan@gmail.com>
2015-11-19 16:01:50 -0500

    Initial import

--- src/main/java/com/theironyard/controllers/MixedDrinksController.java
+++ src/main/java/com/theironyard/controllers/MixedDrinksController.java
@@ -1,7 +1,11 @@
 package com.theironyard.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Jack on 11/19/15.
  */
+
+@RestController
 public class MixedDrinksController {
 }
--- src/main/java/com/theironyard/entities/User.java
+++ src/main/java/com/theironyard/entities/User.java
@@ -1,7 +1,18 @@
 package com.theironyard.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Jack on 11/19/15.
  */
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    int id;
 }
