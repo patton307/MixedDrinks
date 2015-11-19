0f38f58108f3beeb55fc8b581d7da19461a49d58
Blake Guilloud <fbguillo@gmail.com>
2015-11-19 17:34:45 -0500

    setting up blakeBone

---
+++ public/js/collection.js
@@ -0,0 +1,11 @@
+var Backbone = require('backbone');
+
+module.exports = Backbone.Model.extend({
+  url: 'http://addb.absolutdrinks.com/drinks/?apiKey=fa0ea8ba586c4f70b9397e6e64613fa9&start=25&pageSize=3000',
+  initialize: function(){
+    console.log(this.url);
+  },
+  parse: function(data){
+    console.log(data);
+  }
+});
---
+++ public/js/modelView.js
@@ -0,0 +1,12 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+Backbone.$ = $;
+
+module.exports = Backbone.View.extend({
+  tagName: 'section',
+  className: 'drink',
+  initialize: function(){
+    console.log('blue');
+  }
+});
