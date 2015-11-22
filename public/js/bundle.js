8d655a4f9d70c97c828ceeb9472af920ce4e2adb
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-11-22 12:48:25 -0500

    fixing things

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -356,6 +356,7 @@ var UserView = require('./userView');
 var UserCollection = require('./userCollection');
 var UserCollectionView = require('./userCollectionView');
 
+
 module.exports = Backbone.Router.extend ({
   routes: {
     'home': 'homePage',
@@ -369,23 +370,29 @@ module.exports = Backbone.Router.extend ({
   homePage: function(){
     console.log("home page");
     new layoutView();
-    $('#layoutView').find('.box').remove();
-    new FormView();
-  var headerHTML = new HeaderView();
-      $('#layoutView').html(headerHTML.render().el);
-    
-    // var userHTML = new UserView();
-    // $('#layoutView').append(userHTML.render().el);
+
+
+    // $('#layoutView').find('.box').remove();
+    var userHTML = new UserView();
+    $('#layoutView').prepend(userHTML.render().el);
+
+    var formHTML = new FormView();
+    $('.content').html(formHTML.render().el);
+    var headerHTML = new HeaderView();
+    $('.content').html(headerHTML.render().el);
 
   },
   profilePage: function(){
     console.log("profile page");
-    $('#layoutView').find('.drinkform').remove();
+    // $('#layoutView').find('.drinkform').remove();
     $('.content').find('article').remove();
     var headerHTML = new HeaderView();
     $('#layoutView').html(headerHTML.render().el);
     var profileHTML = new ProfileView();
     $('#layoutView').append(profileHTML.render().el);
+    var userHTML = new UserView();
+
+    $('#side').append(userHTML.render().el);
 
 
   },
@@ -395,7 +402,7 @@ module.exports = Backbone.Router.extend ({
   loginPage: function(){
     var loginHTML = new LoginView();
     $('#layoutView').append(loginHTML.render().el);
-    // new layoutView();
+
 
   }
 
