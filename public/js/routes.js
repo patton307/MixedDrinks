4dac200ac5f50143d50fea2a2d887a85eb61ed00
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-11-22 13:31:43 -0500

    fuxed up

--- public/js/routes.js
+++ public/js/routes.js
@@ -2,7 +2,6 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 var _ = require('underscore');
-var headerView = require('./headerView');
 var DrinkCollection = require('./collection');
 var CollectionView = require('./collectionView');
 var LoginView = require('./loginView');
@@ -10,47 +9,36 @@ var FormView = require('./formView');
 var HeaderView = require('./headerView');
 var layoutView = require('./layoutView');
 var ProfileView = require('./profileView');
-var UserView = require('./userView');
 var UserCollection = require('./userCollection');
 var UserCollectionView = require('./userCollectionView');
+var FavoriteCollection = require('./favoritesCollection');
+var FavoritesCollectionView = require('./favoritesView');
 
 
 module.exports = Backbone.Router.extend ({
-  routes: {
-    'home': 'homePage',
-    '': 'loginPage',
-    'profile': 'profilePage',
+ routes: {
+   '': 'loginPage',
+   'home': 'homePage',
+   'profile': 'profilePage',
+ },
+ homePage: function(){
+    var users = new UserCollection();
+    users.fetch().then(function() {
+      new UserCollectionView({collection: users});
+      new layoutView();
+      $('#layoutView').find('.box').remove();
+    });
   },
-  homePage: function(){
+  profilePage: function(){
+    var favorites = new FavoriteCollection();
+    favorites.fetch().then(function(data){
+      console.log('favorites data', data[0]);
+      new FavoritesCollectionView({collection: favorites});
 
-    // $('#layoutView').find('.toTheLeft').removeClass('hidden');
-     console.log("home page");
-     var users = new UserCollection();
-     users.fetch().then(function() {
-       new UserCollectionView({collection: users});
-       new layoutView();
-       $('#layoutView').find('.box').remove();
-
-     });
-   },
-  //  profilePage: function(){
-  //   //  $('#layoutView').find('.toTheLeft').removeClass('hidden');
-  //   //   console.log("profile page");
-  //   //   var headerHTML = new HeaderView();
-  //   //   $('#layoutView').find('section').remove();
-  //   //   var profileHTML = new ProfileView();
-  //   //   $('#layoutView').append(profileHTML.render().el);
-   //
-   //
-  //  },
-  loginPage: function(){
-    var loginHTML = new LoginView();
-    $('#layoutView').append(loginHTML.render().el);
-
-
-  }
-
-
-
-
+    });
+  },
+ loginPage: function(){
+   var loginHTML = new LoginView();
+   $('#layoutView').html(loginHTML.render().el);
+ }
 });
